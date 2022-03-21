7b111e162d4fff6203f3263a0cb0cc96b82076b3
Devon Faria <devonfaria@gmail.com>
2022-03-21 18:45:45 -0400

    created prompts for manager, and linked inquirer module, and function to dynamically create HTML file

--- index.js
+++ index.js
@@ -6,4 +6,53 @@ const fs = require('fs');
 // const internTest = require('./tests/intern.test');
 // const managerTest = require('./tests/manager.test');
 
+const questionsManager = [
+  {
+    type: 'input',
+    message: "What is your manager's name?",
+    name: 'name',
+  },
+  {
+    type: 'input',
+    message: "What is the manager's GitHub user name?",
+    name: 'username',
+  },
+  {
+    type: 'input',
+    message: "What is the manager's email?",
+    name: 'email',
+  },
+  {
+    type: 'input',
+    message: "What is the manager's office number?",
+    name: 'username',
+  },
+  {
+    type: 'input',
+    message: "What is the manager's office ID?",
+    name: 'username',
+  },
+];
 
+// Function that creates an HTML file and adds it to the dist folder in the repository
+function writeToFile(text) {
+  fs.writeFile('./dist/index.html', text, (err) => {
+    err ? console.error(err) : console.log('Success!')
+  })
+};
+
+
+// Initializes the terminal questions
+function promptManager() {
+  inquirer
+  .prompt(questionsManager)
+    .then ((data) => {
+      console.log(data);
+      // may have to add this information to an array/object instead
+      const htmlText = 'generateHTML function passing data';
+      writeToFile(htmlText)
+    })
+    .catch((err) => {console.log(err)});
+};
+
+promptManager();
--- tests/intern.test.js
+++ tests/intern.test.js
@@ -1,1 +1,13 @@
 const index = require('../index');
+
+const intern = {
+  name: 'Devon Faria',
+  email: 'devon@hi.com',
+  officeNum: '1234',
+  empID: '5678',
+  school: 'University of Florida',
+}
+
+describe('', () => {
+  
+})
