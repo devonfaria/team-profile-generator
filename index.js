ab67ad147c6aac353610b8f5d5a1a1bba0b95d91
Devon Faria <devonfaria@gmail.com>
2022-03-21 17:17:34 -0400

    installed npm packages and linked index.js to test scripts

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // Linking required files and dependencies
 const inquirer = require('inquirer');
 const fs = require('fs');
-const employeeTest = require('./tests/employee.test');
-const engineerTest = require('./tests/engineer.test');
-const internTest = require('./tests/intern.test');
-const managerTest = require('./tests/manager.test');
+// const employeeTest = require('./tests/employee.test');
+// const engineerTest = require('./tests/engineer.test');
+// const internTest = require('./tests/intern.test');
+// const managerTest = require('./tests/manager.test');
 
 
