eb636a991f3862cf9245acb14870f88bbb22f27a
Devon Faria <devonfaria@gmail.com>
2022-03-21 21:45:24 -0400

    created employee constructor that accepts data from Inquirer

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 // Linking required files and dependencies
 const inquirer = require('inquirer');
 const fs = require('fs');
-// const employeeTest = require('./tests/employee.test');
-// const engineerTest = require('./tests/engineer.test');
-// const internTest = require('./tests/intern.test');
-// const managerTest = require('./tests/manager.test');
+const Manager = [];
+const Engineer = [];
+const Intern = [];
 
 // QUESTIONS FOR TERMINAL PROMPTS
 const questionsManager = [
@@ -110,6 +109,7 @@ const generateHTML = function () {
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>My Team</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
   <link rel="stylesheet" href="./src/style.css">
 </head>
 <body>
@@ -120,9 +120,9 @@ const generateHTML = function () {
   </main>
 </body>
 </html>`
-}
+};
 
-// GENERATES TEXT FOR HTML PAGE
+// GENERATES CARD FOR HTML PAGE
 const generateCard = function (data) {
   return `<div>
   <div>
@@ -135,9 +135,7 @@ const generateCard = function (data) {
     <p><span>${data.office}</span></p>
   </div>
 </div>`
-}
-// const htmlText = 'generateHTML function passing data';
-// writeToFile(htmlText);
+};
 
 // Initializes the terminal questions
 function promptManager() {
@@ -188,4 +186,10 @@ function addEmployee() {
     .catch((err) => {console.log(err)});
 };
 
+// Initializes prompts
 promptManager();
+
+// const employeeTest = require('./tests/employee.test');
+// const engineerTest = require('./tests/engineer.test');
+// const internTest = require('./tests/intern.test');
+// const managerTest = require('./tests/manager.test');
--- tests/manager.test.js
+++ tests/manager.test.js
@@ -1,1 +1,31 @@
 const index = require('../index');
+
+describe("promptManager", () => {
+  // Positive Tests
+  it("should return an object with properties", () => {
+    // Arrange
+    const todoList = new ;
+    const todoText = "Mow Lawn";
+
+    // Act
+    todoList.addTodo(todoText);
+
+    // Assert
+    expect(todoList.todos.length).toEqual(1);
+    expect(todoList.todos[0] instanceof Todo).toEqual(true);
+    expect(todoList.todos[0].text).toEqual(todoText);
+  });
+
+  // Exception test
+  it("should throw an error if not provided text", () => {
+    // Arrange
+    const todoList = new TodoList();
+    const err = new Error(
+      "Expected parameter 'text' to be a non empty string"
+    );
+    const cb = () => todoList.addTodo();
+
+    // Assert
+    expect(cb).toThrowError(err);
+  });
+});
